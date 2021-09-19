6fa09977a20f8d04b54b7c259c713ee1de54de1c
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-19 13:41:15 +0500

    done

---
+++ newapp.js
@@ -0,0 +1,22 @@
+var interval;
+
+var displayh2=document.getElementById('h2')
+var a=0;
+function timer(){
+    displayh2.innerHTML=a;
+    a++;
+    if(a==40){
+        clearInterval(interval)
+        displayh2.innerHTML=a+'+';
+    }
+}
+function start() {
+    interval = setInterval(timer, 30);
+
+    console.log(millisec)
+    console.log(sec)
+}
+
+window.onload=start()
+
+
